fbef2be270342245956e98f565425255277d77a7
harunmohamed <harunmohamed901@gmail.com>
2021-11-04 16:32:26 +0200

    cabeeapp v1

---
+++ screens/HomeScreen.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
+import NavFavorites from '../components/NavFavorites';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+
+
+const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 230, height: 50, resizeMode: 'contain', marginTop: 5, marginBottom: 15
+                    }} 
+                    source={{
+                        uri: "https://i.imgur.com/rVIhC1g.png",
+                    }}
+                />
+
+                <GooglePlacesAutocomplete 
+                    placeholder="Where From?"
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 22,
+                        },
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(
+                            setOrigin({
+                                location: details.geometry.location,
+                                description: data.description
+                            })
+                        );
+
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={'search'}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                />
+
+                <NavOptions />
+                <NavFavorites />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'blue'
+    }
+})
